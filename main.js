83790d0be0cafc67e728c3eaf26016536b152da8
Rodolfo Spalenza <rodolfo.spalenza@gmail.com>
2017-05-26 20:01:10 -0300

    Set fullscreen and remove zoom.

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  /* mainWindow = new BrowserWindow({width: 800, height: 600}) */
+  mainWindow = new BrowserWindow({fullscreen: true})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+// remove zoom
+const electron = require('electron')
+const webFrame = electron.webFrame;
+
+webFrame.setVisualZoomLevelLimits(1, 1);
+webFrame.setLayoutZoomLevelLimits(0, 0);
